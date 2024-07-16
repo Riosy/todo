e07c374493f9efa8abd5219d153d679d5cbec40f
Riosy <riosycreates@gmail.com>
2024-07-16 21:54:00 +0330

    ADD Contex & Items and mapping Items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import TopForm from './TopForm';
 import TaskItems from './TaskItems';
+import { taskcontext } from './context';
 
 const App = ()=>{
-    return (
+
+const [taskitems , settaskitems] = useState([
+    {
+        id:1 ,
+        title:"کار شماره 1",
+        done : false
+    },
+    {
+        id:2 ,
+        title:"کار شماره 2",
+        done : true
+    },
+    {
+        id:3 ,
+        title:"کار شماره 3",
+        done : false
+    }
+])
+   return (
+    
         <div className="container w-100 h-100 p-3">
             <div className="row h-100 justify-content-center align-align-items-start">
                 <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
+                <taskcontext.Provider value={{
+                    taskitems,
+                    settaskitems
+                }}>
                     <TopForm />
                     <TaskItems />
+                    </taskcontext.Provider>
                 </div>
             </div>
         </div>
---
+++ src/context.js
@@ -0,0 +1,7 @@
+import { createContext } from "react"
+
+
+export const taskcontext = createContext ({
+    taskitems : [],
+    settaskitems : ()=>{}
+})
